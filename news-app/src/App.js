bc13297bac78bc235ac3e1cb7a15435738a25fea
Sumit <sumitj00112233@gmail.com>
2023-01-15 23:48:19 +0530

    file updated

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,51 +1,46 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import AddContact from './components/AddContact';
-import ContactList from './components/ContactList';
+// import AddContact from './components/AddContact';
+// import ContactList from './components/ContactList';
 
-//static
-// const contacts =[
-//   {
-//     id:"1",
-//     name:"sumit",
-//     email:"sumit@gmail.com"
-//   },
-//   {
-//     id:"2",
-//     name:"Amrii",
-//     email:"Amrii@gmail.com"
-//   }
 
 
 function App() {
-  //declare the hook
-  // const state=useState();
-// implementing the hook on the target area
+    const bgDefaultColour = "red";
+    const [bg, setBg] = useState(bgDefaultColour);
+    const [name, setName] = useState("click me");
+    const [change, setChange] = useState("btn btn-success");
 
- 
- const  incFunc =()=>{
-  // updating the num using setCount
-   let newCTime =new Date().toLocaleTimeString();
-  setCtime(newCTime)
+    const bgChange = () => {
 
-  }
-  setInterval(incFunc,1000);
-  let newTime =new Date().toLocaleTimeString();
-  const[cTime,setCtime]=useState(newTime);
+        const newBg = "#8e44ad";
+        setBg(newBg);
+        setName("Fuck U 🤌 ");
 
-  return (
-    <div className="app">
-<h1>{cTime}</h1>
-{/* <button className="btn btn-primary" onClick={incFunc}>Click</button> */}
+    }
+
+    const bgBack = () => {
+        const color = "lightblue"
+        setBg(color);
+        setName("Oops 😯 ");
+        setChange("btn btn-primary")
+
+    }
+
+    return ( <
+        div style = {
+            { backgroundColour: bg } } >
+
+        <
+        button className = { change }
+        onClick = { bgChange }
+        onDoubleClick = { bgBack } > { name } < /button>
+
+        <
+        /div>
+    );
 
 
-{/*      
-       <AddContact/> 
-    <h3>ContactList</h3>
-       <ContactList contacts={contacts}/> */}
-
-    </div>
-  );
 }
 
 export default App;
