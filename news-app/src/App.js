1e8c73086fa6c839fd104c542aa3ac5e91dffbb0
Sumit <sumitj00112233@gmail.com>
2023-01-16 15:46:02 +0530

    new updated file

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
 import './App.css';
-// import AddContact from './components/AddContact';
-// import ContactList from './components/ContactList';
+
 
 
 
 function App() {
+    const[lastName,setLastName]=useState("");
+    const [name,setName]=useState("");
+    const [fullName,setFullName]=useState("");
+    const [lastnameNew,setlastnameNew]=useState("");
 
-    return ( <
-        >
-        <
-        />
-    );
+    const inpEvent =(e)=>{
+        setName(e.target.value);
+    }
+    const inpEventtwo =(e)=>{
+        setLastName(e.target.value);
+
+    }
+    const onSubmit =(e)=>{
+    e.preventDefault();
+        setFullName(name);
+        setlastnameNew(lastName);
+    }
+
+   return(
+    <div className='main_div'>
+        <form onSubmit={onSubmit}>
+        <div>
+        <h1>Hello {fullName} {lastnameNew}</h1>
+        <input className="input-group-text" id="inputGroup-sizing-default"
+        type="text" onChange={inpEvent} value={name}  placeholder="Enter Your Name"></input>
+        <br/>
+        <input className="input-group-text" id="inputGroup-sizing-default"
+        type="text"  onChange={inpEventtwo} value={lastName}  placeholder="Enter Your lastname"></input>
+        <br/>
+        <button type="submit" className='btn btn-primary'>Click me</button>
+    </div>
+        </form>
+    </div>
+
+   );
 }
 
 export default App;
--- news-app/src/index.js
+++ news-app/src/index.js
@@ -5,17 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    React.StrictMode >
-    <
-    App / >
-
-    <
-    /React.StrictMode>
-); <
-script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+root.render(
+    <React.StrictMode >
+    <App/>
+    </React.StrictMode>
+);
+<script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
 integrity = "sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-crossorigin = "anonymous" > < /script>
+crossorigin = "anonymous" > </script>
     // If you want to start measuring performance in your app, pass a function
     // to log results (for example: reportWebVitals(console.log))
     // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
